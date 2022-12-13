6bb9959f35b591b574a3187bf36190ad06fb666b
arima0210 <arima0210@gmail.com>
2022-12-13 23:13:01 +0900

    CharBoardDao에 메서드 추가!

--- CCC/src/main/java/com/kh/ccc/board/charBoard/model/dao/CharBoardDao.java
+++ CCC/src/main/java/com/kh/ccc/board/charBoard/model/dao/CharBoardDao.java
@@ -1,10 +1,15 @@
 package com.kh.ccc.board.charBoard.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class CharBoardDao {
 
 	
+	public int selectListCount(SqlSessionTemplate sqlsession) {
+		
+		return 0;
+	}
 	
 }
