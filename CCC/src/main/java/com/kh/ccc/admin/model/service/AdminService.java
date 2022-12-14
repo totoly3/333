8d1863e05232d7726eea3377f66357454de54e43
3232kk <3232kk@gmail.com>
2022-12-14 19:33:39 +0900

    12/14일까지 작업

---
+++ CCC/src/main/java/com/kh/ccc/admin/model/service/AdminService.java
@@ -0,0 +1,12 @@
+package com.kh.ccc.admin.model.service;
+
+import java.util.ArrayList;
+
+import com.kh.ccc.admin.model.vo.Member;
+
+public interface AdminService {
+	
+	//회원 전체 리스트 조회
+	ArrayList<Member> memberList();
+
+}
