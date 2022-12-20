5cad477195826c926ed102c3396b3ae1d3fbb7e3
Jojinhee91 <hail99jo@naver.com>
2022-12-20 13:33:47 +0900

    중간

--- CCC/src/main/java/com/kh/ccc/board/tipBoard/model/dao/TipBoardDao.java
+++ CCC/src/main/java/com/kh/ccc/board/tipBoard/model/dao/TipBoardDao.java
@@ -1,4 +1,4 @@
-package com.kh.ccc.board.freeboard.model.dao;
+package com.kh.ccc.board.tipBoard.model.dao;
 
 import java.util.ArrayList;
 
@@ -8,49 +8,50 @@ import org.springframework.stereotype.Repository;
 
 import com.kh.ccc.board.freeboard.model.vo.FrBoard;
 import com.kh.ccc.board.freeboard.model.vo.FrBoardAttach;
+import com.kh.ccc.board.tipBoard.model.vo.TipBoard;
 import com.kh.ccc.common.model.vo.PageInfo;
 
 @Repository
-public class FrBoardDao {
+public class TipBoardDao {
 	//게시글 갯수 조회
 	public int selectListCount(SqlSessionTemplate sqlSession) {
 		int result = sqlSession.selectOne("frBoardMapper.selectListCount");
 		return result ;
 	}
 	//아래는 게시글 리스트 조회 
-	public ArrayList<FrBoard> selectList(SqlSessionTemplate sqlSession, PageInfo pi) {
+	public ArrayList<TipBoard> selectList(SqlSessionTemplate sqlSession, PageInfo pi) {
 		int limit = pi.getBoardLimit();
 		int offset =(pi.getCurrentPage()-1)* limit;	
 		
 		RowBounds rowBounds = new RowBounds(offset,limit);
-		ArrayList<FrBoard> flist=(ArrayList)sqlSession.selectList("frBoardMapper.selectList",null,rowBounds);
-		return flist;
+		ArrayList<TipBoard> tlist=(ArrayList)sqlSession.selectList("tipBoardMapper.selectList",null,rowBounds);
+		return tlist;
 	}
 
 	//d아래는 조회수 증가 
-	public int increaseCount(SqlSessionTemplate sqlSession, int fno) {
+	public int increaseCount(SqlSessionTemplate sqlSession, int tno) {
 		
-		return sqlSession.update("frBoardMapper.increaseCount",fno);
+		return sqlSession.update("tipBoardMapper.increaseCount",tno);
 		
 	}
 	//게시물  상세보기
-	public ArrayList<FrBoard> frboardDetailView(SqlSessionTemplate sqlSession, int fno) {
+	public ArrayList<TipBoard> tipboardDetailView(SqlSessionTemplate sqlSession, int tno) {
 
-		ArrayList<FrBoard> frbalist=(ArrayList)sqlSession.selectList("frBoardMapper.frboardDetailView",fno);
-		System.out.println("frbalist :"+frbalist);
-		return frbalist;
+		ArrayList<TipBoard> trbalist=(ArrayList)sqlSession.selectList("tipBoardMapper.tipboardDetailView",fno);
+		System.out.println("tipbalist :"+tipbalist);
+		return tipbalist;
 		
 	}
 	//아래는 게시글 상세보기 첨부파일 가져오려고
 	public FrBoardAttach frboardAttDetailView(SqlSessionTemplate sqlSession, int fno) {
 		
-		return sqlSession.selectOne("frBoardMapper.frboardAttDetailView");
+		return sqlSession.selectOne("tipBoardMapper.frboardAttDetailView");
 	}
 	
 	//아래는 게시글 등록(글만)
-	public int insertFrBoard1(SqlSessionTemplate sqlSession, FrBoard fb) {
+	public int insertFrBoard1(SqlSessionTemplate sqlSession, FrBoard tb) {
 		
-		int result1 =sqlSession.insert("frBoardMapper.insertFrBoard1",fb);
+		int result1 =sqlSession.insert("tipBoardMapper.insertFrBoard1",tb);
 		System.out.println("result1 게시글 등록되었으면 1"+result1);
 		return result1;
 	}
